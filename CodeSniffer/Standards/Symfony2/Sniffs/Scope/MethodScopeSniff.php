f741257dabb63b74762f88f01423fe93410bb89b
Szymon Fraszczak <sfraszczak@n-educatio.pl>
2013-10-02 10:59:15 +0200

    Update Symfony2 standards

--- CodeSniffer/Standards/Symfony2/Sniffs/Scope/MethodScopeSniff.php
+++ CodeSniffer/Standards/Symfony2/Sniffs/Scope/MethodScopeSniff.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Verifies that class members have scope modifiers.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id: MethodScopeSniff.php 301632 2010-07-28 01:57:56Z squiz $
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+if (class_exists('PHP_CodeSniffer_Standards_AbstractScopeSniff', true) === false) {
+    throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_Standards_AbstractScopeSniff not found');
+}
 
 /**
  * Verifies that class members have scope modifiers.
